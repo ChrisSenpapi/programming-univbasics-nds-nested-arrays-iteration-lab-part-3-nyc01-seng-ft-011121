231c18a1db8d0643a96be9a37623bf3980e133e6
Christopher Read <christopherread22@outlook.com>
2020-12-11 21:35:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,11 @@
 def join_nested_strings(src)
-
+new_str = ""
+  src.each do |array|
+    array.each do |element|
+    if element.is_a? String
+       new_str = new_str + " " +element
+     end
+  end
+end
+  new_str
 end
